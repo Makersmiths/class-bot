88fa31750e5a085001ddc877b029d694469d8fc6
Riceman2000 <alex4222000@gmail.com>
2025-07-19 22:11:32 -0400

    Add oauth renewal

--- src/apricot_api.rs
+++ src/apricot_api.rs
@@ -9,8 +9,18 @@ use serde_derive::{Deserialize, Serialize};
 pub enum ApricotError {
     #[error("Error making request")]
     NetError(#[from] reqwest::Error),
+
     #[error("Error parsing request response")]
     ParseError(#[from] serde_json::Error),
+
+    #[error("Environment variable file error")]
+    DotEnvError(#[from] dotenv::Error),
+
+    #[error("Environment variable load error")]
+    EnvVarError(#[from] std::env::VarError),
+
+    #[error("Type conversion parse error")]
+    ConvertError(#[from] std::num::TryFromIntError),
 }
 
 #[derive(Debug)]
@@ -24,9 +34,7 @@ pub struct ApricotApi {
 }
 
 impl ApricotApi {
-    /// Form a Open AI interface, does not make any requests by itself
-    /// # Panics
-    /// Failure to load env vars
+    /// Form a Wild Apricot API interface, does not make any requests by itself
     /// # Errors
     /// Failure to request initial oauth token
     pub async fn new() -> Result<Self, ApricotError> {
@@ -37,15 +45,15 @@ impl ApricotApi {
             || env::var("APRICOT_API_URI").is_err()
             || env::var("APRICOT_AUTH_URI").is_err()
         {
-            dotenv::dotenv().expect("Failed to load env vars for API.");
+            dotenv::dotenv()?;
         }
 
-        let auth_uri: String = env::var("APRICOT_AUTH_URI").expect("Auth URI not defined");
-        let api_uri: String = env::var("APRICOT_API_URI").expect("API URI not defined");
-        let api_key: String = env::var("APRICOT_API_KEY").expect("API Key not defined");
-        let api_key = BASE64_STANDARD.encode(format!("APIKEY:{api_key}"));
+        let auth_uri: String = env::var("APRICOT_AUTH_URI")?;
+        let api_uri: String = env::var("APRICOT_API_URI")?;
+        let api_key: String = env::var("APRICOT_API_KEY")?;
 
-        let auth_header: String = format!("Basic {api_key}");
+        let auth_header = BASE64_STANDARD.encode(format!("APIKEY:{api_key}"));
+        let auth_header: String = format!("Basic {auth_header}");
 
         // Initial oauth request, further requests should just be renewals
         let client = reqwest::Client::new();
@@ -68,13 +76,8 @@ impl ApricotApi {
         // Extract the pieces we care about
         let oauth_token = oauth_response.access_token;
         let account_id = oauth_response.permissions[0].account_id;
-        let token_expire = time::Instant::now()
-            + time::Duration::from_secs(
-                oauth_response
-                    .expires_in
-                    .try_into()
-                    .expect("Could not calculate oauth token expiry"),
-            );
+        let token_expire =
+            time::Instant::now() + time::Duration::from_secs(oauth_response.expires_in.try_into()?);
 
         Ok(Self {
             auth_uri,
@@ -89,11 +92,40 @@ impl ApricotApi {
     /// Request a new token from oauth, replace the old one, update expiry time
     /// # Errors
     /// Errors on API request or parsing issues
-    pub fn renew_token(&mut self) -> Result<(), ApricotError> {
-        self.oauth_token = "wip".to_string();
-        let _ = self.auth_uri;
-        let _ = self.api_key;
-        todo!("Add logic for token renewal");
+    pub async fn renew_token(&mut self) -> Result<(), ApricotError> {
+        info!("Requesting token renewal");
+
+        let auth_header = BASE64_STANDARD.encode(format!("APIKEY:{}", self.api_key));
+        let auth_header: String = format!("Basic {auth_header}");
+
+        let client = reqwest::Client::new();
+        let resp = client
+            .post(format!("{}/auth/token", self.auth_uri))
+            .header(
+                reqwest::header::CONTENT_TYPE,
+                "application/x-www-form-urlencoded",
+            )
+            .header(reqwest::header::AUTHORIZATION, auth_header)
+            .body("grant_type=client_credentials&scope=auto&obtain_refresh_token=true")
+            .send()
+            .await?
+            .text()
+            .await?;
+        trace!("Raw renewal response: {resp}");
+        let oauth_response: OAuthResponse = serde_json::from_str(&resp)?;
+        debug!("Renewal response: {oauth_response:?}");
+
+        // Extract the pieces we care about
+        let oauth_token = oauth_response.access_token;
+        let account_id = oauth_response.permissions[0].account_id;
+        let token_expire =
+            time::Instant::now() + time::Duration::from_secs(oauth_response.expires_in.try_into()?);
+
+        self.oauth_token = oauth_token;
+        self.account_id = account_id;
+        self.token_expire = token_expire;
+
+        Ok(())
     }
 
     /// Get list of events on the calendar
@@ -103,7 +135,7 @@ impl ApricotApi {
         info!("Events requested");
         if self.token_expire < time::Instant::now() {
             info!("oauth token has expired since last request, getting a renewal");
-            self.renew_token()?;
+            self.renew_token().await?;
         }
 
         let client = reqwest::Client::new();
@@ -187,6 +219,19 @@ mod tests {
     async fn new_apricot() {
         let apricot = ApricotApi::new().await.unwrap();
         assert!(!apricot.oauth_token.is_empty());
+    }
+
+    #[tokio::test]
+    #[test_log::test]
+    async fn refresh_token() {
+        let mut apricot = ApricotApi::new().await.unwrap();
+        let oauth_key = apricot.oauth_token.clone();
+        apricot
+            .renew_token()
+            .await
+            .expect("Failed to refresh token");
+        let new_oauth_key = apricot.oauth_token;
+        assert_ne!(oauth_key, new_oauth_key, "Key was not updated");
     }
 
     #[tokio::test]
